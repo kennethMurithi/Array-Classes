0fb3d2b11dbc0ae82852ad17315b851784ac1a9f
kennethMurithi <kennethmmuriithi@gmail.com>
2018-06-05 18:47:11 +0300

    Merge origin/initial_company_view into initial_company_view
    Conflicts:
    	src/main/java/finextremehr/entity/personalManagement/Employees.java
    	src/main/webapp/user_management.xhtml

--- src/array/classes/ArrayClasses.java
+++ src/array/classes/ArrayClasses.java
@@ -15,7 +15,30 @@ public class ArrayClasses {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         // student input array(more tipically input at runtime)
+        int[] responses = {1,2,5,4,3,5,2,1,3,3,1,4,3,3,3,2,3,3,2,14};
+        
+        //array of frequency counter
+        int[] frequency = new int[6];
+        
+        //for each answer, select responses element and use that value 
+        //as frequency index to detemine element to increment
+        for (int answer = 0; answer < responses.length; answer++){
+            try{
+                ++frequency[responses[answer]];
+            }//end try
+            catch(ArrayIndexOutOfBoundsException e)
+                    {
+                        System.out.println(e);
+                        System.out.printf("responses[%d] = %d\n\n", answer, responses[answer] );
+            }//end catch
+        } //end for
+        
+        System.out.printf("%s10s\n", "Rating", "Frequency");
+        //output each array elements value;
+        for (int rating = 1; rating < frequency.length; rating++){
+            System.out.printf("%6d%10d\n", rating, frequency[rating]);
+        }
     }
     
 }
